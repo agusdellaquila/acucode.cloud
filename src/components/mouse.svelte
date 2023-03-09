090f74c16b4ad9bff125d1f4413e4d16cb5dfb3e
agusdellaquila <agusdellaquila72@gmail.com>
2023-03-09 02:48:27 -0300

    svelte layout and background set

---
+++ src/components/mouse.svelte
@@ -0,0 +1,65 @@
+<script>
+    import { onMount } from 'svelte';
+    import "../app.css";
+  
+    let blob;
+    
+  
+    onMount(() => {
+      /* -- Glow effect -- */
+      window.onpointermove = event => { 
+        const { clientX, clientY } = event;
+          
+        blob.animate({
+          left: `${clientX}px`,
+          top: `${clientY}px`
+        }, { duration: 3000, fill: "forwards" });
+      }
+    });
+</script>
+  
+
+
+<div id="blob" bind:this={blob}></div>
+    <div id="blur"></div>
+
+
+<style>
+    @keyframes rotate {
+        from {
+            rotate: 0deg;
+        }
+        
+        50% {
+            scale: 1 1.5;
+        }
+        
+        to {
+            rotate: 360deg;
+        }
+    }
+
+    #blob {
+        background-color: white;
+        height: 26vmax;
+        aspect-ratio: 1;
+        position: absolute;
+        left: 50%;
+        top: 50%;
+        translate: -50% -50%;
+        border-radius: 50%;
+        background: linear-gradient(to right, rgb(255, 206, 127), rgb(242, 167, 46));
+        animation: rotate 20s infinite;
+        opacity: 0.9;
+        overflow: hidden;
+    }
+
+    #blur {
+        height: 100%;
+        width: 100%;
+        position: absolute;
+        z-index: 2;
+        backdrop-filter: blur(12vmax);
+        overflow: hidden;
+    }
+</style>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,18 @@
+<script>
+  import "../app.css"
+</script>
+
+<div class="background">
+  <slot> </slot>
+</div>
+
+
+<style>
+	.background {
+		background: linear-gradient(180deg, #82ADDB 0%, #757ABF 54.17%, #191621 100%);
+		color: white;
+		height: 100vh;
+		margin: 0rem;
+		overflow: hidden; 
+	}
+</style>
